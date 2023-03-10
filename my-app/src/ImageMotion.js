23116fa0d5174b7f2c8af8e3aa4db93a0baeb823
Aigerim Suleimenova <aigerim.suleimenova@nu.edu.kz>
2023-03-10 20:42:00 +0600

    test

--- my-app/src/ImageMotion.js
+++ my-app/src/ImageMotion.js
@@ -1,45 +1,19 @@
 import React, { useState} from 'react';
-import {motion} from 'framer-motion';
+const requireImages = require.context('../assets/genplan/first/images', false, /\.jpg$/);
+
 function ImageMotion() {  
   const [currentIndex, setIndex] = useState(0);
   const [direction, setDirection] = useState(0);
  
   const images1 = [];
-  const images2 = [];
-  const images3 = [];
-  
-  const requireImages = require.context('../assets/genplan/first/images', false, /\.jpg$/);
-  const requireImages2 = require.context('../assets/genplan/second/images', false, /\.jpg$/);
-  const requireImages3 = require.context('../assets/genplan/third/images', false, /\.jpg$/);
+
+
 
   requireImages.keys().forEach(filename => {
     images1.push(requireImages(filename));
   });
-  requireImages2.keys().forEach(filename => {
-    images2.push(requireImages(filename));
-  });
-  requireImages3.keys().forEach(filename => {
-    images3.push(requireImages(filename));
-  });
 
-const variants = {
-  initial: (direction) =>{
-   return {
-      x: direction > 0 ? 200 : -200,
-      opacity: 0
-    }
-  },
-  animate: {
-    x: 0,
-    opacity: 1,
-  }, 
-  exit: direction =>{
-    return{
-    x: direction >0 ? -200 : 200,
-    opacity: 0,
-  }
-}
-}
+
 
   function nextstep(){
     setDirection(1)
@@ -68,12 +42,7 @@ const variants = {
   return (
     <div className="container">
       <div className='slideshow'>
-        <motion.img
-          custom={direction}
-          variants={variants}
-          animate = "animate"
-          initial = "initial"
-          exit = "exit"
+        <img
           src={images1[currentIndex]}
         />
 
